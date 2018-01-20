143322bdff286e2aefcd2fc139d732b70beaffbf
Anthony Ma <akma327@stanford.edu>
2018-01-20 18:25:02 -0500

    Normalized data

--- src/stroke_pca.py
+++ src/stroke_pca.py
@@ -5,10 +5,13 @@ import numpy as np
 from numpy import genfromtxt
 import pandas as pd
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
+
 
 stroke_data = "../data/IST_cleaned.csv"
 variable_labels = "../data/IST_variables.csv"
+normalized_stroke_data = "../data/IST_normalized.csv"
 
 def process_data():
 	"""
@@ -28,10 +31,18 @@ def stroke_pca():
 	Perform a pca on the the filtered dataset. 
 	"""
 	header, data = process_data()
-	X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-	pca = PCA(n_components=30)
-	pca.fit_transform(data)
-	print(pca.components_)
+
+	data_std = StandardScaler().fit_transform(data)
+	f = open(normalized_stroke_data, 'w')
+	f.write(",".join(header))
+	for line in data_std:
+		f.write(",".join(map(str,line)) + "\n")
+
+
+	print data_std, data_std.shape, data.shape
+	# pca = PCA(n_components=30)
+	# pca.fit_transform(data)
+	# print(pca.components_)
 	# print(pca.explained_variance_ratio_) 
 
 if __name__ == "__main__":
