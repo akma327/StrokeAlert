89b5e253a3135acb31ac39e54a1e1e86816bafca
Anthony Ma <akma327@stanford.edu>
2018-01-20 20:14:03 -0500

    Finished balancing data set

--- src/balance_data.py
+++ src/balance_data.py
@@ -9,11 +9,23 @@ python balance_data.py /Users/anthony/Desktop/hackathon/StrokeAlert/data/cause_o
 
 def balance_data(input_file, output_file):
 	f = open(input_file, 'r')
+	fo = open(output_file, 'w')
 	header = f.readline()
 	alive_count, dead_count = 0, 0
+	max_count = 5000
 	for line in f:
 		linfo = line.strip().split(",")
-		print linfo
+		life_state = int(linfo[-1])
+		if(life_state == 1): 
+			if(alive_count < max_count):
+				fo.write(line)
+				alive_count +=1
+		else:
+			if(dead_count < max_count):
+				fo.write(line)
+				dead_count +=1
+	
+
 
 if __name__ == "__main__":
 	(input_file, output_file) = (sys.argv[1], sys.argv[2])
