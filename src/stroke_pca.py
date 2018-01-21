7be3026be39979d3998696fec32982c7d834b88d
Anthony Ma <akma327@stanford.edu>
2018-01-20 19:30:56 -0500

    Pruned and normalized data set and output y variables for predicting secondary outcome of stroke. Push into neural network

--- src/stroke_pca.py
+++ src/stroke_pca.py
@@ -8,42 +8,54 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
+USAGE_STR = """
+
+python stroke_pca.py /Users/anthony/Desktop/hackathon/StrokeAlert/data/cause_of_death/x_features.csv /Users/anthony/Desktop/hackathon/StrokeAlert/data/cause_of_death/x_features_normalized.csv
+"""
+
 
 stroke_data = "../data/IST_cleaned.csv"
 variable_labels = "../data/IST_variables.csv"
 normalized_stroke_data = "../data/IST_normalized.csv"
 
-def process_data():
+def process_data(input_file):
 	"""
 	Parse the complete stroke trial database and retain relevant columns. 
 	Data cleaning phase
 	"""
-	f = open(stroke_data, 'r')
+	f = open(input_file, 'r')
 	header = f.readline().split(",")
 	data = []
 	for line in f:
 		data.append(map(float, line.split(",")))
 	return header, np.array(data)
 
+def normalize_data(input_file, output_file):
+	header, data = process_data(input_file)
+
+	data_std = StandardScaler().fit_transform(data)
+	f = open(output_file, 'w')
+	f.write(",".join(header))
+	for line in data_std:
+		f.write(",".join(map(str,line)) + "\n")
+
+	return data_std
 
 def stroke_pca():
 	"""
 	Perform a pca on the the filtered dataset. 
 	"""
-	header, data = process_data()
+	header, data = process_data(stroke_data)
 
-	data_std = StandardScaler().fit_transform(data)
-	f = open(normalized_stroke_data, 'w')
-	f.write(",".join(header))
-	for line in data_std:
-		f.write(",".join(map(str,line)) + "\n")
-
-
-	print data_std, data_std.shape, data.shape
+	data_std = normalize_data(input_file, output_file)
+	
 	# pca = PCA(n_components=30)
 	# pca.fit_transform(data)
 	# print(pca.components_)
 	# print(pca.explained_variance_ratio_) 
 
 if __name__ == "__main__":
-	stroke_pca()
+	(input_file, output_file) = (sys.argv[1], sys.argv[2])
+	normalize_data(input_file, output_file)
+	# stroke_pca()
+
